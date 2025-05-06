a2ee71eac7e65d38c2991bbf5998cc3294041b2f
Elvis <igieborelvis@gmail.com>
2025-05-06 21:51:18 +0100

    fixed auth issues

--- lib/src/config/providers.dart
+++ lib/src/config/providers.dart
@@ -1,9 +1,10 @@
+import 'package:get_it/get_it.dart';
 import 'package:itestified/src/config/donation_history_provider.dart';
 import 'package:itestified/src/config/theme/app_theme.dart';
 
 import 'package:itestified/src/features/app_theme/theme_viewmodel.dart';
+import 'package:itestified/src/features/auth/presentation/viewmodel/auth_viewmodel.dart';
 import 'package:itestified/src/features/nav/navbar.dart';
-
 
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
@@ -15,18 +16,20 @@ final List<SingleChildWidget> providers = [
   //   create: (_) => GetIt.I<RegisterViewModel>(),
   // ),
 
-  // ChangeNotifierProvider<ThemeViewmodel>(
-  //   create: (_) => GetIt.I<ThemeViewmodel>(),
+  ChangeNotifierProvider<ThemeViewmodel>(
+    create: (_) => GetIt.I<ThemeViewmodel>(),
+  ),
+
+  ChangeNotifierProvider<AuthViewModel>(
+    create: (_) => GetIt.I<AuthViewModel>(),
+  ),
+  // ChangeNotifierProvider(
+  //   create: (_) => ThemeViewmodel(),
+
   // ),
   ChangeNotifierProvider(
-    create: (_) => ThemeViewmodel(),
-    
-  ),
-    ChangeNotifierProvider(
     create: (_) => AuthProvider(),
-
-    
   ),
   ChangeNotifierProvider(create: (_) => DonationHistoryProvider()),
-      ChangeNotifierProvider(create: (_) => NavProvider())
+  ChangeNotifierProvider(create: (_) => NavProvider())
 ];
