9873c2320b29c67a1e4a6bcebd1f045e9e65de77
Pixel <MacBook@MacBook-Pro-2.local>
2025-05-06 08:51:50 -0700

    change password enhancement

--- lib/src/core/widgets/btn_and_text.dart
+++ lib/src/core/widgets/btn_and_text.dart
@@ -1,40 +1,65 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:itestified/src/config/theme/app_color.dart';
 import 'package:itestified/src/core/widgets/text_widget.dart';
 
-Widget btnAndText(
-    {double containerWidth = 200,
-    double horizontalPadding = 10,
-    double verticalPadding = 5,
-    Color containerColor = AppColors.btnColor,
-    String text = 'Next',
-    Color borderColor = AppColors.btnColor,
-    Color textColor = AppColors.white,
-    double? fontSize = 17,
-    Widget container = const SizedBox.shrink()}) {
+Widget btnAndText({
+  double containerWidth = 200,
+  double horizontalPadding = 10,
+  double verticalPadding = 5,
+  Color containerColor = AppColors.btnColor,
+  String text = 'Next',
+  Color borderColor = AppColors.btnColor,
+  Color textColor = AppColors.white,
+  double? fontSize = 17,
+  Widget container = const SizedBox.shrink(),
+  bool isLoading = false, 
+}) {
   return Container(
     width: containerWidth,
-    // height: 50,
     padding: EdgeInsets.symmetric(
-        horizontal: horizontalPadding, vertical: verticalPadding),
+      horizontal: horizontalPadding,
+      vertical: verticalPadding,
+    ),
     decoration: BoxDecoration(
       border: Border.all(width: 1, color: borderColor),
       borderRadius: BorderRadius.circular(5),
       color: containerColor,
     ),
-    child: Center(
-        child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    child: Stack(
+      alignment: Alignment.center,
       children: [
-        container,
-        textWidget(
-          text,
-          color: textColor,
-          fontSize: fontSize,
+       
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            container,
+            if (isLoading) 
+          Container(
+            width: 20,
+            height: 20,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: AppColors.grey50,
+            ),
+            child: const CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              strokeWidth: 1.5,
+              backgroundColor: Colors.transparent,
+              strokeCap: StrokeCap.round,
+            ),
+          ),
+          const SizedBox(width: 4,),
+            textWidget(
+              text,
+              color: textColor,
+              fontSize: fontSize,
+            ),
+          ],
         ),
+
+        
       ],
-    )),
+    ),
   );
 }
 
--- lib/src/core/widgets/custom_textfield.dart
+++ lib/src/core/widgets/custom_textfield.dart
@@ -3,109 +3,146 @@ import 'package:itestified/src/config/theme/app_color.dart';
 import 'package:itestified/src/core/widgets/normal_text_style.dart';
 import 'package:itestified/src/features/app_theme/theme_viewmodel.dart';
 import 'package:itestified/src/features/auth/presentation/viewmodel/auth_viewmodel.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
 import 'package:provider/provider.dart';
 
 class customTextField extends StatefulWidget {
-  customTextField(
-      {super.key,
-      this.prefixIc,
-      this.borderColor = AppColors.lightBlack,
-      this.borderWidth = 1,
-      this.suffixIcon = const SizedBox.shrink(),
-      this.hintText = "Enter your full name",
-      this.validate,
-      this.controller,
-      this.authViewModel,
-      this.errorText,
-      this.onChange,
-      this.isLast = false,
-      this.obscureText = false});
-  String hintText = "Enter your full name";
-  Widget? prefixIc;
-  Widget suffixIcon;
-  double borderWidth;
-  Color borderColor;
-  String? Function(String? value)? validate;
-  TextEditingController? controller;
-  AuthViewModel? authViewModel;
-  bool? isLast;
-  bool obscureText;
+  customTextField({
+    super.key,
+    this.prefixIc,
+    this.borderColor = AppColors.lightBlack,
+    this.borderWidth = 1,
+    this.suffixIcon = const SizedBox.shrink(),
+    this.hintText = "Enter your full name",
+    this.validate,
+    this.controller,
+    this.authViewModel,
+    this.errorText,
+    this.onChange,
+    this.isLast = false,
+    this.obscureText = false,
+    this.suffixIconEnabled = false,
+  });
 
-  String? errorText;
-  String? Function(String? value)? onChange;
+  final String hintText;
+  final Widget? prefixIc;
+  final Widget suffixIcon;
+  final double borderWidth;
+  final Color borderColor;
+  final String? Function(String? value)? validate;
+  final TextEditingController? controller;
+  final AuthViewModel? authViewModel;
+  final bool? isLast;
+  final bool obscureText;
+  final bool suffixIconEnabled;
+  final String? errorText;
+  final String? Function(String? value)? onChange;
+
   @override
   State<customTextField> createState() => _customTextFieldState();
 }
 
 class _customTextFieldState extends State<customTextField> {
-  // String? _errorMessage;
+  bool _isObscured = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _isObscured = widget.obscureText;
+  }
+
   @override
   Widget build(BuildContext context) {
     var themeProvider = Provider.of<ThemeViewmodel>(context);
     TextInputAction checkLast =
         widget.isLast == true ? TextInputAction.done : TextInputAction.next;
-    return Column(
-      
-      children: [
-      TextFormField(
-        obscureText: widget.obscureText,
-        textInputAction: checkLast,
-        controller: widget.controller,
-        validator: widget.validate,
-        onChanged: widget.onChange,
-        style: normalTextStyle(
-            fontSize: Theme.of(context).textTheme.bodyLarge?.fontSize,
-            textColor: themeProvider.themeData.colorScheme.tertiary),
-        decoration: InputDecoration(
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(
-                width: widget.borderWidth, color: widget.borderColor),
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(
-                width: widget.borderWidth, color: widget.borderColor),
-          ),
-          suffixIcon: widget.suffixIcon,
-          prefixIcon: widget.prefixIc,
-          contentPadding:
-              const EdgeInsets.symmetric(vertical: 10, horizontal: 13),
-          hintText: widget.hintText,
-          hintStyle: normalTextStyle(
-            textColor: themeProvider.themeData.colorScheme.onTertiary,
-            fontSize: Theme.of(context).textTheme.bodyLarge?.fontSize,
-          ),
-          focusedErrorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide:
-                BorderSide(width: widget.borderWidth, color: Colors.white),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(
-                width: widget.borderWidth, color: widget.borderColor),
-          ),
-          filled: true,
-          error: null,
-          errorStyle: const TextStyle(height: 0, fontSize: 0),
-          fillColor: themeProvider.themeData.colorScheme.outline,
-        ),
-      ),
-      if (widget.errorText != null) ...[
-        const SizedBox(height: 5),
-        Row(
-          children: [
-            const Icon(Icons.error_outline, color: Colors.red, size: 18),
-            const SizedBox(width: 5),
-            Text(
-              widget.errorText ?? '',
-              style: const TextStyle(color: Colors.red, fontSize: 14),
+
+    Widget effectiveSuffixIcon = widget.suffixIconEnabled
+        ? IconButton(
+            icon: Icon(
+              _isObscured ? Symbols.visibility_off : Symbols.visibility,
+              color: themeProvider.themeData.colorScheme.onTertiary,
             ),
-          ],
-        )
-      ]
-    ]);
+            onPressed: () {
+              setState(() {
+                _isObscured = !_isObscured;
+              });
+            },
+          )
+        : widget.suffixIcon;
+
+    return Column(
+      children: [
+        TextFormField(
+          obscureText:
+              widget.suffixIconEnabled ? _isObscured : widget.obscureText,
+          textInputAction: checkLast,
+          controller: widget.controller,
+          validator: widget.validate,
+          onChanged: widget.onChange,
+          style: normalTextStyle(
+            fontSize: Theme.of(context).textTheme.bodyLarge?.fontSize,
+            textColor: themeProvider.themeData.colorScheme.tertiary,
+          ),
+          decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                width: widget.borderWidth,
+                color: widget.borderColor,
+              ),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                width: widget.borderWidth,
+                color: widget.borderColor,
+              ),
+            ),
+            suffixIcon: effectiveSuffixIcon,
+            prefixIcon: widget.prefixIc,
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 10, horizontal: 13),
+            hintText: widget.hintText,
+            hintStyle: normalTextStyle(
+              textColor: themeProvider.themeData.colorScheme.onTertiary,
+              fontSize: Theme.of(context).textTheme.bodyLarge?.fontSize,
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                width: widget.borderWidth,
+                color: Colors.white,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                width: widget.borderWidth,
+                color: widget.borderColor,
+              ),
+            ),
+            filled: true,
+            error: null,
+            errorStyle: const TextStyle(height: 0, fontSize: 0),
+            fillColor: themeProvider.themeData.colorScheme.outline,
+          ),
+        ),
+        if (widget.errorText != null) ...[
+          const SizedBox(height: 5),
+          Row(
+            children: [
+              const Icon(Icons.error_outline, color: Colors.red, size: 18),
+              const SizedBox(width: 5),
+              Text(
+                widget.errorText ?? '',
+                style: const TextStyle(color: Colors.red, fontSize: 14),
+              ),
+            ],
+          )
+        ]
+      ],
+    );
   }
 }
 
